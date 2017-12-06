2d5eaaf1abf8408cb2d46f2f397b5848df4df145
adi766 <adityakumarteli7846@gmail.com>
2017-12-06 06:35:20 +0000

    Revert "can: kvaser_usb: Retry the first bulk transfer on -ETIMEDOUT"
    This reverts commit 4cd574b9234d1741a1813a6b5cce0c0f371db8a9.

--- drivers/net/can/usb/kvaser_usb.c
+++ drivers/net/can/usb/kvaser_usb.c
@@ -1578,7 +1578,7 @@ static int kvaser_usb_probe(struct usb_interface *intf,
 {
 	struct kvaser_usb *dev;
 	int err = -ENOMEM;
-	int i, retry = 3;
+	int i;
 
 	dev = devm_kzalloc(&intf->dev, sizeof(*dev), GFP_KERNEL);
 	if (!dev)
@@ -1596,15 +1596,7 @@ static int kvaser_usb_probe(struct usb_interface *intf,
 
 	usb_set_intfdata(intf, dev);
 
-	/* On some x86 laptops, plugging a Kvaser device again after
-	 * an unplug makes the firmware always ignore the very first
-	 * command. For such a case, provide some room for retries
-	 * instead of completely exiting the driver.
-	 */
-	do {
-		err = kvaser_usb_get_software_info(dev);
-	} while (--retry && err == -ETIMEDOUT);
-
+	err = kvaser_usb_get_software_info(dev);
 	if (err) {
 		dev_err(&intf->dev,
 			"Cannot get software infos, error %d\n", err);
