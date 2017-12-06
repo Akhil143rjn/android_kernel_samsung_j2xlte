ad4fe3e211e453049632b17e2e8cb97ddb6a9d75
adi766 <adityakumarteli7846@gmail.com>
2017-12-06 06:01:45 +0000

    Revert "x86, tls, ldt: Stop checking lm in LDT_empty"
    This reverts commit aac9117d37d1e07d352502dd1d0cdfbccac93c37.

--- arch/x86/include/asm/desc.h
+++ arch/x86/include/asm/desc.h
@@ -251,8 +251,7 @@ static inline void native_load_tls(struct thread_struct *t, unsigned int cpu)
 		gdt[GDT_ENTRY_TLS_MIN + i] = t->tls_array[i];
 }
 
-/* This intentionally ignores lm, since 32-bit apps don't have that field. */
-#define LDT_empty(info)					\
+#define _LDT_empty(info)				\
 	((info)->base_addr		== 0	&&	\
 	 (info)->limit			== 0	&&	\
 	 (info)->contents		== 0	&&	\
@@ -261,6 +260,12 @@ static inline void native_load_tls(struct thread_struct *t, unsigned int cpu)
 	 (info)->limit_in_pages		== 0	&&	\
 	 (info)->seg_not_present	== 1	&&	\
 	 (info)->useable		== 0)
+
+#ifdef CONFIG_X86_64
+#define LDT_empty(info) (_LDT_empty(info) && ((info)->lm == 0))
+#else
+#define LDT_empty(info) (_LDT_empty(info))
+#endif
 
 static inline void clear_LDT(void)
 {
